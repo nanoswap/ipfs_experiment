a7eea7f9a7d9781b763e986057d7349c387a0afc
Nate Schultz <nate.schultz@outlook.com>
2023-03-20 17:26:30 -0400

    misc cleanup

--- src/crud.py
+++ src/crud.py
@@ -3,38 +3,42 @@ import ipfs
 import utils
 
 import nanoswap.message.identity_pb2 as identity_pb2
-from models.return_types import NewCreditIdentity, NewLookup
+import nanoswap.message.lookup_pb2 as lookup_pb2
+from models.return_types import CreditId, CreditIdStatus
 
-def write_identity(id_content: str, id_type: int) -> NewCreditIdentity:
+def get_credit_id(identity: identity_pb2.Identity) -> CreditId:
     """
-    Create a new credit identity and write it to ipfs
-
+    For the given identity (id_value, id_type), either:
+        1. retrieve the credit id corresponding to this identity
+    or:
+        2. create a new credit id for this identity
+    
     Args:
         id_content (str): The value of the id
         id_type (int): The kind of id
 
     Returns:
-        NewCreditIdentity: The generated credit_id and it's corresponding data
+        CreditId: The credit_id for the identity and metadata about if it is new to the system
     """
-    identity = utils.new_identity(
-        id_type,
-        id_content
-    )
-    ipfs.write(identity.filename, identity.data)
-    return identity
+    filename = utils.get_filename(identity)
+    print(filename)
 
-def write_lookup(identity: identity_pb2.Identity, credit_id: uuid.UUID) -> NewLookup:
-    """
-    Create the lookup data corresponding to a credit identity.
-    Add that lookup data to ipfs.
+    # check if the identity already exists
+    file_exists = ipfs.does_file_exist(filename)
+    if not file_exists:
 
-    Args:
-        identity (identity_pb2.Identity): The credit identity data to generate lookup data from
-        credit_id (uuid.UUID): The credit identity uuid corresponding to the identity object
+        # generate a new credit id
+        credit_id = uuid.uuid4()
 
-    Returns:
-        NewLookup: The generated filename and the lookup data content
-    """
-    lookup = utils.new_lookup(identity, credit_id)
-    ipfs.write(lookup.filename, lookup.data)
-    return lookup
+        # wrap it in a protobuf and write it to ipfs
+        ipfs.write(filename, lookup_pb2.Lookup(
+            credit_identity = str(credit_id)
+        ))
+
+        return CreditId(credit_id, CreditIdStatus.CREATED)
+    
+    else:
+
+        # read the existing identity
+        ipfs_data = ipfs.read(filename, lookup_pb2.Lookup())
+        return CreditId(uuid.UUID(ipfs_data.credit_identity), CreditIdStatus.RETRIEVED)
--- src/models/return_types.py
+++ src/models/return_types.py
@@ -1,16 +1,14 @@
 from dataclasses import dataclass
 from uuid import UUID
 
-import nanoswap.message.identity_pb2 as identity_pb2
 import nanoswap.message.lookup_pb2 as lookup_pb2
+from enum import Enum
+
+class CreditIdStatus(Enum):
+    CREATED = 0
+    RETRIEVED = 1
 
 @dataclass
-class NewCreditIdentity:
-    credit_id: UUID
-    filename: str
-    data: identity_pb2.Identity
-
-@dataclass
-class NewLookup:
-    filename: str
-    data: lookup_pb2.Lookup
+class CreditId:
+    id: UUID
+    status: CreditIdStatus
