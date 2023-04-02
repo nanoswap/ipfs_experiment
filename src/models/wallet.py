a39ca4e51bd7bd1c5225effc3d9a399c84105225
Nate Schultz <nate.schultz@outlook.com>
2023-04-02 19:54:14 -0400

    moved generate_private_key

--- src/models/wallet.py
+++ src/models/wallet.py
@@ -1,4 +1,5 @@
 from utils import nano
+import subprocess
 
 class Account():
     
@@ -23,6 +24,13 @@ class Account():
 #         assert balance(self.wallet) >= amount
        nano.send(self.wallet, self.account, destination, amount)
     
+    @staticmethod
+    def generate_private_key():
+        command = "LC_ALL=C cat /dev/urandom | LC_ALL=C tr -dc '0-9A-F' | LC_ALL=C head -c${1:-64}"
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+        return process.stdout.read()
+
+
     @staticmethod
     async def receivable_promise(wallet, account):
         while True:
--- src/utils/nano.py
+++ src/utils/nano.py
@@ -1,13 +1,7 @@
 import requests
-import subprocess
 
 rpc_network = "http://127.0.0.1:17076"
 session = requests.Session()
-
-def generate_private_key():
-    command = "LC_ALL=C cat /dev/urandom | LC_ALL=C tr -dc '0-9A-F' | LC_ALL=C head -c${1:-64}"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-    return process.stdout.read()
 
 def key_expand(key):
     return session.post(rpc_network, json={
